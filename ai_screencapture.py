0d225798dc0459bab289fef36353b2a993ed3e8b
edtepes <edtepes@gmail.com>
2025-02-02 00:06:18 -0800

    added constant screen display 1ms delay and q exit key

--- ai_screencapture.py
+++ ai_screencapture.py
@@ -7,13 +7,16 @@ print("Screen Resolution: " + "w: " + str(w) + " h:" + str(h))
 img = None
 monitor = {"top":0, "left": 0, "width": w, "height": h}
 with mss.mss() as sct:
-    img = sct.grab(monitor)
-    img  = np.array(img) #takes that data and converts it to numpy array. Converting from rgb to bgr is not needed thanks to the grab function, since screenshot apps and opencv both work in bgr
+    while True:
+        img = sct.grab(monitor)
+        img  = np.array(img) #takes that data and converts it to numpy array. Converting from rgb to bgr is not needed thanks to the grab function, since screenshot apps and opencv both work in bgr
 
-    small = cv.resize(img, (0, 0), fx = 0.5, fy = 0.5) #small version of the screen image (a resize using scalars)
+        small = cv.resize(img, (0, 0), fx = 0.5, fy = 0.5) #small version of the screen image (a resize using scalars)
 
-    cv.imshow("Computer Vision", small) #displaying the image on the screen
+        cv.imshow("Computer Vision", small) #displaying the image on the screen
 
-    cv.waitKey(0) #introducing a delay so that we can see the screen or it will disapear to quickly
+        key = cv.waitKey(1) #introducing a 1ms delay so that we can see the screen or it will disapear to quickly
+        if key == ord('q'):
+            break
 
-    cv.destroyAllWindows()
+cv.destroyAllWindows()
